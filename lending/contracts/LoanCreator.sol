7ef4f28ea9b5ca9136eda46cd17b37e8c91764cc
Rcontre360 <rcontreraspimentel@gmail.com>
2022-04-09 08:44:38 -0400

    added credit cards contract

---
+++ lending/contracts/LoanCreator.sol
@@ -0,0 +1,288 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+pragma abicoder v2;
+
+import {RedirectAll, ISuperToken, IConstantFlowAgreementV1, ISuperfluid} from "./RedirectAll.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "./TradeableFlowStorage.sol";
+
+// Ideas:
+// - make LOC size relative to income rate
+
+contract LoanCreator is ERC721, ERC721URIStorage, RedirectAll {
+
+    using Strings for uint256;                                                    // clever package which lets you cast uints to strings
+    using Counters for Counters.Counter;
+    Counters.Counter tokenIds;
+
+    string public baseURI;
+    string public cid;
+
+    event LOCOpened(address employee);
+
+    constructor (
+        address _owner,
+        string memory _name,
+        string memory _symbol,
+        string memory _baseURI,
+        ISuperToken _paymentToken,
+        IERC20 _lendingToken,              // Stablecoin that will be lent out on LOCs
+        int96 _interestRate,               // Number between 100 and 200, so 30% APR is 130
+        uint256 _locAmount,
+        ISuperfluid host,
+        IConstantFlowAgreementV1 cfa,
+        string memory registrationKey
+    )
+        public ERC721 ( _name, _symbol )
+        RedirectAll (
+        host,
+        cfa,
+        _owner,
+        registrationKey
+        )
+    { 
+        _scp.owner = _owner;
+        _scp.paymentToken = _paymentToken;
+        _scp.lendingToken = _lendingToken;
+        _scp.interestRate = _interestRate;
+        _scp.locAmount = _locAmount;
+        baseURI = _baseURI;
+        // cid = _cid;
+    }
+
+    function mint() public returns (uint256 tokenId) {
+        // require employer registered the employee under them
+        require(_scp.employees[msg.sender].employer != address(0), "!employed");
+        // require employee doesn't already have a card
+        require(_scp.employees[msg.sender].tokenId == 0, "alreadyHasCard");
+
+        // Increment token ID
+        tokenIds.increment();
+        tokenId = tokenIds.current();
+
+        _mint(msg.sender,tokenId);
+
+        // set the token ID for the employee (msg.sender)
+        _scp.tokenIdToEmployee[tokenId] = msg.sender;
+        _scp.employees[msg.sender].tokenId = tokenId;
+
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721) {
+        require(to != _scp.owner,"!own");
+
+        //this should be added
+        super._beforeTokenTransfer(from, to, tokenId);
+
+        if (from != address(0)) {
+            _changeReceiver(from, to, tokenId);
+        }
+    }
+
+    // Employee "stakes" their income and can now borrow
+    function openLoc() public {
+        // Employee must have minted supercard
+        require(_scp.employees[msg.sender].tokenId != 0, "!mintedSC");
+        
+        _scp.employees[msg.sender].locOpen = true;
+        _scp.employees[msg.sender].availableCredit = _scp.locAmount;
+
+        emit LOCOpened(msg.sender);
+    }
+
+    function closeLoc() public {
+        require(_scp.employees[msg.sender].availableCredit == _scp.locAmount, "stillOutstandingBal");
+        _scp.employees[msg.sender].locOpen = false;
+        _scp.employees[msg.sender].availableCredit = 0;
+    }
+
+    function changeEmployerRegistration(address employer) public {
+        // allows an employer to open an income stream and set registered employees
+        require(msg.sender == _scp.owner,"!owner");
+        
+        _scp.employers[employer].authorized = !_scp.employers[employer].authorized;
+    }
+    
+    // lets an employer sets n employee under him
+    function registerEmployee(address employee) public {
+        // block another employer from registering an already registered employee
+        require(_scp.employers[msg.sender].authorized,"!registeredEmployer");
+        // Sets employee to active in EmployerProfile
+        _scp.employers[msg.sender].activeEmployees[employee] = true;
+        // Push new employee to list
+        _scp.employers[msg.sender].employeeList.push(employee);
+        // Sets employer in EmployeeProfile
+        _scp.employees[employee].employer = msg.sender;
+
+    }
+
+    // TODO: conso requires
+    function borrow(uint256 borrowAmount) public {
+        // Caller must have SuperCard minted
+        require(_scp.employees[msg.sender].tokenId != 0, "!mintedSC");
+        // Caller must locOpen
+        require(_scp.employees[msg.sender].locOpen, "!openLoc");
+        // Caller must be employed
+        require(_scp.employees[msg.sender].employer != address(0), "!employed");
+        // Caller must have an imcome coming through
+        require(_scp.employees[msg.sender].incomeInflowRate != 0, "!income");
+        // Amount can't be greater than available LOC
+        require(borrowAmount <= _scp.employees[msg.sender].availableCredit, "!borrowTooMuch");
+
+        // Calculate new amount of credit to be utilized
+        uint256 newUtilization = (_scp.locAmount - (_scp.employees[msg.sender].availableCredit - borrowAmount));
+        // Calculate repayment rate
+        // repayment rate is such that principle will be repayed over 3 months plus interest reframed to that 3-month timeframe
+        int96 repaymentRate =  int96( ( int( uint( newUtilization ) ) * (_scp.interestRate) / ( 100000 * 3 ) ) / ( 30 * 24 * 60 * 60 ) );
+        int96 repaymentDelta = repaymentRate - _scp.employees[msg.sender].interestOutflowRate;
+        _scp.employees[msg.sender].interestOutflowRate = repaymentRate;
+
+        // modify existing flow to employee
+        (,int96 currentFlowToEmployee,,) = _scp.cfa.getFlow(_scp.paymentToken, address(this), msg.sender);
+        _updateFlow(msg.sender, currentFlowToEmployee - repaymentDelta,_scp.paymentToken);
+
+        // Start/update repayment flow to program owner
+        (,int96 currentInterestFlow,,) = _scp.cfa.getFlow(_scp.paymentToken, address(this), _scp.owner);
+        if (currentInterestFlow == 0) {
+            _createFlow(_scp.owner, currentInterestFlow + repaymentDelta, _scp.paymentToken);
+        } else {
+            _updateFlow(_scp.owner, currentInterestFlow + repaymentDelta, _scp.paymentToken);
+        }
+
+        // Borrow Amount
+        IERC20(_scp.lendingToken).transfer(msg.sender, borrowAmount);
+
+        // Update availableLoc amount
+        _scp.employees[msg.sender].availableCredit -= borrowAmount;
+    }
+
+    function repay(uint256 repayAmount) public {
+        // Caller must be employed
+        require(_scp.employees[msg.sender].employer != address(0), "!employed");
+        // not repaying too much
+        require(repayAmount <= (_scp.locAmount -_scp.employees[msg.sender].availableCredit), "!tooMuchRepay");
+
+        // Calculate new amount of credit to be utilized
+        uint256 newUtilization = (_scp.locAmount - (_scp.employees[msg.sender].availableCredit + repayAmount));
+        // Calculate repayment rate
+        // repayment rate is such that principle will be repayed over 3 months plus interest reframed to that 3-month timeframe
+        int96 repaymentRate =  int96( ( int( uint( newUtilization ) ) * (_scp.interestRate) / ( 100000 * 3 ) ) / ( 30 * 24 * 60 * 60 ) );
+        int96 repaymentDelta = repaymentRate - _scp.employees[msg.sender].interestOutflowRate;
+        _scp.employees[msg.sender].interestOutflowRate = repaymentRate;
+
+        // modify existing flow to employee
+        (,int96 currentFlowToEmployee,,) = _scp.cfa.getFlow(_scp.paymentToken, address(this), msg.sender);
+        _updateFlow(msg.sender, currentFlowToEmployee - repaymentDelta,_scp.paymentToken);
+
+        // update repayment flow to program owner
+        (,int96 currentInterestFlow,,) = _scp.cfa.getFlow(_scp.paymentToken, address(this), _scp.owner);
+        // if (currentInterestFlow == 0) {
+        //     _createFlow(_scp.owner, currentInterestFlow + repaymentDelta, _scp.paymentToken);
+        // } 
+        if (currentInterestFlow + repaymentDelta != 0) {
+            _updateFlow(_scp.owner, currentInterestFlow + repaymentDelta, _scp.paymentToken);
+        }
+        else {
+            _deleteFlow(address(this), _scp.owner, _scp.paymentToken);
+        }
+
+        // Transfer in repayAmount
+        IERC20(_scp.lendingToken).transferFrom(msg.sender,address(this), repayAmount);
+        
+        // Update availableLoc amount
+        _scp.employees[msg.sender].availableCredit += repayAmount;
+
+    }
+
+    function setCID(string memory newCID) public {
+        cid = newCID;
+    }
+
+    /********************************************** */
+    /* Getters                                      */
+    /********************************************** */
+
+    function getInterestRate() external view returns (int96) {
+        return _scp.interestRate;
+    }
+
+    function getLocAmount() external view returns (uint256) {
+        return _scp.locAmount;
+    }
+
+    function getLendingToken() external view returns (IERC20) {
+        return _scp.lendingToken;
+    }
+
+    function getPaymentToken() external view returns (ISuperToken) {
+        return _scp.paymentToken;
+    }
+
+    function getTokenIdFromEmployee(address employee) external view returns (uint256) {
+        return _scp.employees[employee].tokenId;
+    }
+
+    function getAvailableCreditFromEmployee(address employee) external view returns (uint256) {
+        return _scp.employees[employee].availableCredit;
+    }
+
+    function getLocStatusFromEmployee(address employee) external view returns (bool) {
+        return _scp.employees[employee].locOpen;
+    }
+
+    function getEmployerFromEmployee(address employee) external view returns (address) {
+        return _scp.employees[employee].employer;
+    }
+
+    /********************************************** */
+    /* ERC721 Overrides                             */
+    /********************************************** */
+
+    /**
+    @dev overriding _burn due duplication in inherited ERC721 and ERC721URIStorage
+    */
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function _baseURI() internal view virtual override(ERC721) returns (string memory) {
+        return baseURI;
+    }
+
+        
+    /**
+    @notice Overrides tokenURI
+    @param tokenId token ID of Drip NFT being queried
+    @return token URI
+    */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        require(_exists(tokenId),"!exist");
+        if (bytes(_baseURI()).length > 0) {
+            return string(
+                abi.encodePacked(
+                _baseURI(),
+                cid
+                )
+            );
+        } else {
+            return "";
+        }
+    }
+
+}
---
+++ lending/contracts/RedirectAll.sol
@@ -0,0 +1,370 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+pragma abicoder v2;
+
+import {
+    ISuperfluid,
+    ISuperToken,
+    ISuperApp,
+    ISuperAgreement,
+    SuperAppDefinitions
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import {
+    IConstantFlowAgreementV1
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+
+import {
+    SuperAppBase
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+
+import "./TradeableFlowStorage.sol";
+
+contract RedirectAll is SuperAppBase {
+
+    TradeableFlowStorage.SuperCardProgram internal _scp;
+    TradeableFlowStorage.TempContextData internal _tcd;
+
+    constructor(
+        ISuperfluid host,
+        IConstantFlowAgreementV1 cfa,
+        address owner,
+        string memory registrationKey
+        ) {
+        require(address(host) != address(0), "host");
+        require(address(cfa) != address(0), "cfa");
+        require(address(owner) != address(0), "owner");
+        require(!host.isApp(ISuperApp(owner)), "owner SA");
+
+        _scp.host = host;
+        _scp.cfa = cfa;
+        _scp.owner = owner;
+
+        uint256 configWord =
+            SuperAppDefinitions.APP_LEVEL_FINAL |
+            SuperAppDefinitions.BEFORE_AGREEMENT_CREATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_UPDATED_NOOP |
+            SuperAppDefinitions.BEFORE_AGREEMENT_TERMINATED_NOOP;
+
+        // _scp.host.registerApp(configWord);
+        if(bytes(registrationKey).length > 0) {
+            _scp.host.registerAppWithKey(configWord, registrationKey);
+        } else {
+            _scp.host.registerApp(configWord);
+        }
+    }
+
+    // To be eligible for income:
+    // 1. Employer must have been registered by SuperCard program owner
+    // 2. Employer must have registerEmployee()
+    // 3. Registered employee must have minted SuperCard
+    // Now employer may start paying employee
+    function _createOutflow(ISuperToken supertoken) internal returns (bytes memory newCtx) {
+        newCtx = _tcd.ctx;
+        // Get employer (flow starter) from agreementData
+        (address employer, ) = abi.decode(_tcd.agreementData, (address, address));
+        // 1. require creator is an authorized employer
+        require(_scp.employers[employer].authorized,"!authorizedEmployer");
+        // Get user data from context (employee) - because of this, the createFlow must be done with userData specified or it will revert
+        // address employee = abi.decode(_scp.host.decodeCtx(newCtx).userData, (address));
+        uint256 tokenId = abi.decode(_scp.host.decodeCtx(newCtx).userData, (uint256));
+        address employee = _scp.tokenIdToEmployee[tokenId];
+        // 2. require target employee is under employer
+        require(_scp.employers[employer].activeEmployees[employee],"!activeEmployee");
+        // 3. require that target employee has actually minted SuperCard
+        require( _scp.employees[employee].tokenId != 0, "!mintedSC" );
+
+
+        // Get how much employer is streaming to the app as income
+        (,int96 currentEmployerFlow,,) = _scp.cfa.getFlow(supertoken, employer, address(this));
+        // Open flow with that same amount to the first employee
+        newCtx = _createFlow(employee,currentEmployerFlow,supertoken,newCtx);
+        // Set the employee's income rate to what the employer is now providing
+        _scp.employees[employee].incomeInflowRate = currentEmployerFlow;
+        
+
+    }
+
+
+    function _updateOutflow(ISuperToken supertoken) internal returns (bytes memory newCtx) {
+        newCtx = _tcd.ctx;
+        // Get employer (flow starter) from agreementData
+        (address employer, ) = abi.decode(_tcd.agreementData, (address, address));
+        // iterate across all of the employer's employees and cancel outbound streams, set employees to inactive
+        // Get user data from context (employee) - because of this, the createFlow must be done with userData specified or it will revert
+        // address employee = abi.decode(_scphost.decodeCtx(newCtx).userData, (address));
+        uint256 tokenId = abi.decode(_scp.host.decodeCtx(newCtx).userData, (uint256));
+
+        // Get employee behind token ID
+        address employee = _scp.tokenIdToEmployee[tokenId];
+
+        // Change flow to employee by rate adjustment delta
+        (,int96 currentEmployerFlow,,) = _scp.cfa.getFlow(supertoken, employer, address(this));
+        (,int96 currentEmployeeFlow,,) = _scp.cfa.getFlow(supertoken, address(this), employee);
+        int96 rateDelta = currentEmployerFlow - _scp.employees[employee].incomeInflowRate;
+        newCtx = _updateFlow(employee, currentEmployeeFlow + rateDelta,_scp.paymentToken, newCtx);
+
+        _scp.employees[employee].incomeInflowRate += rateDelta;
+
+    }
+
+
+    function _deleteOutflow(ISuperToken supertoken) internal returns (bytes memory newCtx) {
+        newCtx = _tcd.ctx;
+        // Get employer (flow starter) from agreementData
+        (address employer, ) = abi.decode(_tcd.agreementData, (address, address));
+
+
+        for (uint i=0; i<_scp.employers[employer].employeeList.length; i++) {
+            address employee = _scp.employers[employer].employeeList[i];
+
+            // Delete flow to employee
+            (,int96 currentFlowToEmployee,,) = _scp.cfa.getFlow(supertoken, address(this), employee);
+            if ( currentFlowToEmployee != 0 ) {
+                newCtx = _deleteFlow(address(this), employee, _scp.paymentToken, newCtx);
+            }
+
+            // if employee has active interest payment going on, cancel it
+            (,int96 currentTotalInterestFlow,,) = _scp.cfa.getFlow(supertoken, address(this), _scp.owner);
+            if ( _scp.employees[employee].interestOutflowRate != 0) {
+                // If reducing by this flow brings it to zero, do a deleteFlow
+                if ( currentTotalInterestFlow - _scp.employees[employee].interestOutflowRate == 0 ) {
+                    newCtx = _deleteFlow(address(this), _scp.owner, _scp.paymentToken, newCtx);
+                } else {
+                    newCtx = _updateFlow(_scp.owner, currentTotalInterestFlow - _scp.employees[employee].interestOutflowRate, _scp.paymentToken, newCtx);
+                }
+            }
+
+            // delete each employee in employees
+            delete _scp.employees[employee];
+
+            // set employee in activeEmployees to false
+            _scp.employers[employer].activeEmployees[employee] = false;
+
+            // set employer to authorized to false
+            _scp.employers[employer].authorized = false;
+            
+        }
+
+        // delete employer from employers
+        delete _scp.employers[employer];
+
+    }
+
+
+    function _changeReceiver( address oldEmployee, address newEmployee, uint tokenId ) internal {
+        // === DATA CHANGES ===
+        // change activeEmployees for the employer that the oldEmployee is under (turn off old one, turn on new one)
+        address employer = _scp.employees[oldEmployee].employer;
+        _scp.employers[employer].activeEmployees[oldEmployee] = false;
+        _scp.employers[employer].activeEmployees[newEmployee] = true;
+        // Add newEmployee to employer employeeList. The old employee will remain, but lack of flow will be accounted for in _deleteOutflow hook
+        _scp.employers[employer].employeeList.push(newEmployee);
+        // make a new employee profile based on the previous one
+        _scp.employees[newEmployee] = _scp.employees[oldEmployee];
+
+        
+        
+        // delete the old employee profile
+        delete _scp.employees[oldEmployee];
+        // change tokenIdToEmployee
+        _scp.tokenIdToEmployee[tokenId] = newEmployee;
+
+        // === FLOW CHANGES ===
+        // Get flow rate to old employee
+        (,int96 oldEmployeeOutflow,,) = _scp.cfa.getFlow(_scp.paymentToken, address(this), oldEmployee);
+        // delete flow to oldEmployee
+        _deleteFlow(address(this), oldEmployee, _scp.paymentToken);
+        // start flow to newEmployee equal to old flow rate
+        _createFlow(newEmployee, oldEmployeeOutflow, _scp.paymentToken);
+    }
+
+
+
+    function afterAgreementCreated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32, // _agreementId,
+        bytes calldata _agreementData,
+        bytes calldata ,// _cbdata,
+        bytes calldata _ctx
+    )
+        external override
+        onlyExpected(_superToken, _agreementClass)
+        onlyHost
+        returns (bytes memory newCtx)
+    {
+
+        _tcd.agreementData = _agreementData;
+        _tcd.ctx = _ctx;
+
+        return _createOutflow(_superToken);
+
+    }
+
+
+    function afterAgreementUpdated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32 ,//_agreementId,
+        bytes calldata _agreementData,
+        bytes calldata ,//_cbdata,
+        bytes calldata _ctx
+    ) external override onlyExpected(_superToken, _agreementClass)
+        onlyHost
+        returns (bytes memory newCtx)
+    {
+
+        _tcd.agreementData = _agreementData;
+        _tcd.ctx = _ctx;
+
+        return _updateOutflow(_superToken);
+
+    }
+
+
+    function afterAgreementTerminated(
+        ISuperToken _superToken,
+        address _agreementClass,
+        bytes32 ,//_agreementId,
+        bytes calldata _agreementData,
+        bytes calldata ,//_cbdata,
+        bytes calldata _ctx
+    )
+        external override
+        onlyHost
+        returns (bytes memory newCtx)
+    {
+
+        _tcd.agreementData = _agreementData;
+        _tcd.ctx = _ctx;
+
+        return _deleteOutflow(_superToken);
+
+    }
+
+
+    function _isValidToken(ISuperToken superToken) private view returns (bool) {
+        return _scp.paymentToken == superToken;
+    }
+
+    function _isCFAv1(address agreementClass) private view returns (bool) {
+        return ISuperAgreement(agreementClass).agreementType()
+            == keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1");
+    }
+
+    modifier onlyHost() {
+        require(msg.sender == address(_scp.host), "RedirectAll: support only one host");
+        _;
+    }
+
+
+    modifier onlyExpected(ISuperToken superToken, address agreementClass) {
+        require(_isValidToken(superToken), "RedirectAll: not accepted token");
+        require(_isCFAv1(agreementClass), "RedirectAll: only CFAv1 supported");
+        _;
+    }
+
+    function _createFlow(
+        address to,
+        int96 flowRate,
+        ISuperToken _superToken,
+        bytes memory ctx
+    ) internal returns (bytes memory newCtx) {
+        (newCtx, ) = _scp.host.callAgreementWithContext(
+            _scp.cfa,
+            abi.encodeWithSelector(
+                _scp.cfa.createFlow.selector,
+                _superToken,
+                to,
+                flowRate,
+                new bytes(0) // placeholder
+            ),
+            "0x",
+            ctx
+        );
+    }
+
+    function _createFlow(address to, int96 flowRate, ISuperToken _superToken) internal {
+       _scp.host.callAgreement(
+           _scp.cfa,
+           abi.encodeWithSelector(
+               _scp.cfa.createFlow.selector,
+               _superToken,
+               to,
+               flowRate,
+               new bytes(0) // placeholder
+           ),
+           "0x"
+       );
+    }
+
+    function _updateFlow(
+        address to,
+        int96 flowRate,
+        ISuperToken _superToken,
+        bytes memory ctx
+    ) internal returns (bytes memory newCtx) {
+        (newCtx, ) = _scp.host.callAgreementWithContext(
+            _scp.cfa,
+            abi.encodeWithSelector(
+                _scp.cfa.updateFlow.selector,
+                _superToken,
+                to,
+                flowRate,
+                new bytes(0) // placeholder
+            ),
+            "0x",
+            ctx
+        );
+    }
+
+    function _updateFlow(address to, int96 flowRate, ISuperToken _superToken) internal {
+        _scp.host.callAgreement(
+            _scp.cfa,
+            abi.encodeWithSelector(
+                _scp.cfa.updateFlow.selector,
+                _superToken,
+                to,
+                flowRate,
+                new bytes(0) // placeholder
+            ),
+            "0x"
+        );
+    }
+
+    function _deleteFlow(
+        address from,
+        address to,
+        ISuperToken _superToken,
+        bytes memory ctx
+    ) internal returns (bytes memory newCtx) {
+        (newCtx, ) = _scp.host.callAgreementWithContext(
+            _scp.cfa,
+            abi.encodeWithSelector(
+                _scp.cfa.deleteFlow.selector,
+                _superToken,
+                from,
+                to,
+                new bytes(0) // placeholder
+            ),
+            "0x",
+            ctx
+        );
+    }
+
+    function _deleteFlow(address from, address to, ISuperToken _superToken) internal {
+        _scp.host.callAgreement(
+            _scp.cfa,
+            abi.encodeWithSelector(
+                _scp.cfa.deleteFlow.selector,
+                _superToken,
+                from,
+                to,
+                new bytes(0) // placeholder
+            ),
+            "0x"
+        );
+    }
+
+}
